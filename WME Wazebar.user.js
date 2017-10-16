14851cd43a047439991969a7f98cde1523d7fd5a
Joe <crazycaveman@users.noreply.github.com>
2017-10-16 11:08:12 -0400

    Update WME Wazebar.user.js

--- WME Wazebar.user.js
+++ WME Wazebar.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Wazebar
 // @namespace    https://greasyfork.org/users/30701-justins83-waze
-// @version      0.4.09ccm3
+// @version      0.4.09ccm4
 // @description  Displays a bar at the top of the editor that displays inbox, forum & wiki links
 // @author       JustinS83
 // @include      https://beta.waze.com/*
